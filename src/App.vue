7cda1a3c6e35bf7f5524930dbe5d7ca2b44c5171
Kiri <kiri@icantw.com>
2025-07-17 17:53:34 +0800

    新增axios套件、連接MongoDB測試

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const comments = ref([])
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:3000/api/comments')
+  comments.value = res.data
+})
+</script>
 
 <template>
   <div>
-    <nav class="p-4 space-x-4">
-      <router-link to="/">首頁</router-link>
-      <router-link to="/about">關於</router-link>
-      <router-link to="/dashboard">儀表板</router-link>
-    </nav>
-    <router-view />
+    <h1>評論清單</h1>
+    <ul>
+      <li v-for="comment in comments" :key="comment._id">
+        <b>{{ comment.name }}</b>：{{ comment.text }}
+      </li>
+    </ul>
   </div>
 </template>
-
-<style scoped>
-nav {
-  width: 100%;
-  height: 70px;
-  background: aliceblue;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-}
-</style>
